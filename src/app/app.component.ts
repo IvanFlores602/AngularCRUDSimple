93f0f4100f3def575f8f14969d512903cbc244bb
IvanFlores602 <ivaanlopez602@gmail.com>
2023-06-23 17:33:53 -0600

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { Employee } from './models/employee';
+import { NgModel } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  employeeArray: Employee[]=[
+    {id:1, name:"juan", country:"USA"},
+    {id:2, name:"Elena", country:"Espeña"},
+    {id:3, name:"Francisco", country:"Colombia"},
+    {id:4, name:"Maria", country:"Japon"},
+  ];
+
+  selectedEmployee: Employee = new Employee();
+  addOrEdit(){
+    if(this.selectedEmployee.id === 0){
+          this.selectedEmployee.id = this.employeeArray.length +1;
+          this.employeeArray.push(this.selectedEmployee);
+
+    }
+
+    
+    this.selectedEmployee = new Employee();
+  }
+
+  openForEdit(employee: Employee){
+    this.selectedEmployee = employee;
+  }
+  delet(){
+    this.employeeArray = this.employeeArray.filter(x => x !=this.selectedEmployee);
+    this.selectedEmployee = new Employee();
+  }
+
+}
